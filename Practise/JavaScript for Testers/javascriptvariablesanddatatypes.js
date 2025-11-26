741de98b6f6b5abc83e5f37af93e05f2daa7d51b
venkatsai494 <venkatsai494@gmail.com>
2025-11-26 07:16:24 +0530

    Adding javascriptvariablesanddatatypes.js

---
+++ Practise/JavaScript for Testers/javascriptvariablesanddatatypes.js
@@ -0,0 +1,12 @@
+var a;//declaring variable
+a=10;//assigning value to variable
+var b=20;//declaring and assigning value to variable
+var sum=a+b;//adding two variables and storing in another variable
+console.log("Sum of a and b is: "+sum);//printing the sum in console
+
+document.write("Sum of a and b is: "+sum+"<br>");//printing the sum on the webpage
+
+var i =10, j=5;//declaring and assigning multiple variables in a single line
+var difference = i - j;//subtracting two variables and storing in another variable
+console.log("Difference of i and j is: "+difference);//printing the difference in console
+
